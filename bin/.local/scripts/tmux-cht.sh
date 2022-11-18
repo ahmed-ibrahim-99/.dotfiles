57d0e9b557184981643cd340b947a7aecc17ba2c
ahmed-ibrahim-99 <a.ibrahim.abdellatif@gmail.com>
2022-11-18 14:31:10 +0200

    TMUX CHT.SH initial

---
+++ bin/.local/scripts/tmux-cht.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    tmux split-window -p 40 -fh bash -c "curl cht.sh/$selected/$query | less"
+else
+    tmux split-window -p 40 -fh bash -c "curl cht.sh/$selected~$query | less"
+fi
